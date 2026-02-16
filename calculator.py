082ff85962c864c70e82608e50de31c6ac7fdba9
Jesús Sánchez <perchu0909@gmail.com>
2026-02-16 00:05:41 -0500

    Agregar 2 operaciones

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a-b
+
+def multiplicacion(a,b):
+    return a*b
+
+def division(a,b):
+    if b==0:
+        print("ERROR: No se puede dividir entre 0")
+    else:
+        return a/b
